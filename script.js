e13b498d2b5453f8abf9c214cb9ba8c7fc7b082b
Joaolinde <146900098+Joaolinde@users.noreply.github.com>
2025-04-03 08:10:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-:root
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-background-color: rgb(255,0,0)
-background-collor: #FFFFFF
-text-decoration-color:
-border-color:
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+});
